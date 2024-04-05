4d27cedc04aacf26dceb0ad54a56ecd7d6d0ee1c
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2024-04-05 18:21:18 -0500

    Update g2_dummy.jl

--- g2_dummy.jl
+++ g2_dummy.jl
@@ -8,22 +8,23 @@ MC_steps = 100000
 N = 1000
 
 #define a 1D vector with beads either pointing Up or down.
-global mx = [(-1)^rand(rng, Int64) for i in 1:N]
+global mx = [rand(1:100) for i in 1:N]
 
 #list of delay times
-delay_times = 10*collect(1:500)
+delay_times = collect(1:10:5001)
 
 #-------------------------------------------------------------------#
 #monte carlo step
 function One_MC()
     global r = rand(1:N)
-    global mx[r] = (-1)*mx[r]
+    global mx[r] += (rand() < 0.5 ? 0 : 1)*((-1)^rand(rng, Int64))*rand(1:10)
 end
 
 #-------------------------------------------------------------------#
 #auto-correlation functio 
 function correlation()
-    global mx_mux += mx_t .* mx_delt
+    global cross_intensity_mx += mx_t .* mx_delt
+    global self_intensity_mx += mx_t
 end
 
 #-------------------------------------------------------------------#
@@ -33,10 +34,15 @@ global auto_correlation = zeros(length(delay_times), 1)
 #-------------------------------------------------------------------#
 #main body
 for delay in eachindex(delay_times)
-    global c = 0
-    global step_count = 0
 
-    global mx_mux = zeros(N,1)
+    global mx = [rand(1:100) for i in 1:N]
+
+    global c = 0
+    global interval_count = 0
+
+    global cross_intensity_mx = zeros(N,1)
+    global self_intensity_mx = zeros(N,1)
+
     global mx_t = mx[1:N]
     for i in 1:MC_steps
         
@@ -49,13 +55,15 @@ for delay in eachindex(delay_times)
             correlation()
 
             global c = 0
-            global step_count += 1
+            global interval_count += 1
             global mx_t = mx[1:N]
 
         end
     end
-    println(step_count)
-    global auto_correlation[delay] = sum(mx_mux)/(step_count*N)
+    println(interval_count)
+    global g2_value = (cross_intensity_mx/interval_count) ./ ((self_intensity_mx/interval_count) .^ 2)
+    global auto_correlation[delay] = sum(g2_value)/N
 end
 
-plot(delay_times, auto_correlation, framestyle=:box, )
+plot(delay_times, auto_correlation, framestyle=:box, xscale=:log10, xlabel="Time delay", ylabel="Auto-correlation", legend=false)
+title!("Model#2 (delta_I: +10 : -10)")
