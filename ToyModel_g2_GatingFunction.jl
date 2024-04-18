7ba26d3958aa6b1a9d525f2b763fcc9f1a236077
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2024-04-18 15:08:31 -0500

    Update ToyModel_g2_GatingFunction.jl

--- ToyModel_g2_GatingFunction.jl
+++ ToyModel_g2_GatingFunction.jl
@@ -2,7 +2,7 @@ using CUDA, Random, Plots, FFTW
 
 rng = MersenneTwister(1234)
 #total number of Monte Carlo steps
-MC_steps = 100000
+MC_steps = 200000
 
 #define the size of a 1 system
 global N = 100
@@ -14,15 +14,15 @@ global mx = [rand(1:50) for i in 1:N]
 
 #list of delay times
 delay_times_1 = collect(1:9)
-delay_times_2 = Array{Int64}(reshape(collect(1:0.5:9.5) .* (10 .^ collect(1:4))', 18*4, 1))
-#delay_times_3 = Array{Int64}(collect(1:0.5:5) .* 10^5)
-global delay_times = vcat(delay_times_1, delay_times_2)
+delay_times_2 = Array{Int64}(reshape(collect(1:0.5:9.5) .* (10 .^ collect(1:2))', 18*2, 1))
+delay_times_3 = Array{Int64}(reshape(collect(1:0.1:9.9) .* (10 .^ collect(3:4))', 90*2, 1))
+global delay_times = vcat(delay_times_1, delay_times_2, delay_times_3)
 
 #define the grating function
-global grating_period = 10000
-x = collect(1:(grating_period))
+global gating_period = 1000
+x = collect(1:(gating_period))
 
-gating_function = 1 .- exp.((x .- grating_period) ./ 1000)
+gating_function = 1 .- exp.((x .- gating_period) ./ 100)
 gating_function = repeat(gating_function, (MC_steps/(gating_period) |> Int64)+1 ,1)
 global gating_function = gating_function[1:MC_steps]'
 
@@ -40,9 +40,12 @@ end
 
 #-------------------------------------------------------------------#
 #auto-correlation functio 
-function correlation()
+function correlation_numerator()
     global cross_intensity_mx += mx_t .* mx_delt
-    global self_intensity_mx += mx_t
+end
+
+function correlation_denominator()
+    global self_intensity_mx += mx
 end
 
 #-------------------------------------------------------------------#
@@ -66,27 +69,39 @@ global auto_correlation = zeros(length(delay_times), 1) |> Array
 
 #------------------------------------------------------------------------------------------------------------------------------#
 #calculating Auto-correlation
+global self_intensity_mx = sum(mx_MCSteps, dims=2)
+
 for delay in eachindex(delay_times)
 
     global interval_count = 0
 
     global cross_intensity_mx = zeros(N,1)
-    global self_intensity_mx = zeros(N,1)
 
     for i in 1:MC_steps
         global mx_t = mx_MCSteps[:,i]
 
         if ((i+delay_times[delay]) <= MC_steps)
             global mx_delt = mx_MCSteps[:, i+delay_times[delay]]
-            correlation()
+            correlation_numerator()
 
             global interval_count += 1
         end
     end
     println(interval_count)
-    global g2_value = (cross_intensity_mx/interval_count) ./ ((self_intensity_mx/interval_count) .^ 2)
+    global g2_value = (cross_intensity_mx/interval_count) ./ ((self_intensity_mx/MC_steps) .^ 2)
     global auto_correlation[delay] = sum(g2_value)/N
 end
 
-plot(delay_times, auto_correlation, framestyle=:box, xscale=:log10, linewidth=2, xlabel="Time delay", ylabel="Auto-correlation", legend=false)
+plot(delay_times, auto_correlation, framestyle=:box, xscale=:log10, 
+    linewidth=2, xlabel="Time delay", ylabel="Auto-correlation", legend=false,
+    minorticks=10, minorgrid=true)
 title!("Model#3")
+
+#savefig("AC_ToyModel_N$(N)_Grating1K.png")
+
+#SAVING THE GENERATED DATA
+#open("AC_ToyModel_N$(N)_Grating1K.txt", "w") do io 					#creating a file to save data
+#    for i in 1:length(delay_times)
+#       println(io,i,"\t", delay_times[i],"\t", auto_correlation[i])
+#    end
+#end
