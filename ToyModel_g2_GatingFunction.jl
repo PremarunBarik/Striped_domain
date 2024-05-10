0c5dc8a36e13317ed215730f6b1ef22a5289a746
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2024-05-10 15:07:01 -0500

    Update ToyModel_g2_GatingFunction.jl

--- ToyModel_g2_GatingFunction.jl
+++ ToyModel_g2_GatingFunction.jl
@@ -10,57 +10,63 @@ global N = 100
 #global replica_num = 10
 
 #define a 1D vector with beads either pointing Up or down.
-global mx = [rand(1:50) for i in 1:N]
+global mx = rand(1:25)
 
 #list of delay times
-delay_times_1 = collect(1:9)
-delay_times_2 = Array{Int64}(reshape(collect(1:0.5:9.5) .* (10 .^ collect(1:2))', 18*2, 1))
-delay_times_3 = Array{Int64}(reshape(collect(1:0.1:9.9) .* (10 .^ collect(3:4))', 90*2, 1))
-global delay_times = vcat(delay_times_1, delay_times_2, delay_times_3)
+#delay_times_1 = collect(1:9)
+#delay_times_2 = Array{Int64}(reshape(collect(1:0.5:9.5) .* (10 .^ collect(1:2))', 18*2, 1))
+#delay_times_3 = Array{Int64}(reshape(collect(1:0.1:9.9) .* (10 .^ collect(3:4))', 90*2, 1))
+#global delay_times = vcat(delay_times_1, delay_times_2, delay_times_3)
+global delay_times = collect(1:1000)
 
 #define the grating function
-global gating_period = 1000
+global gating_period = 200
 x = collect(1:(gating_period))
 
 gating_function = 1 .- exp.((x .- gating_period) ./ 100)
 gating_function = repeat(gating_function, (MC_steps/(gating_period) |> Int64)+1 ,1)
 global gating_function = gating_function[1:MC_steps]'
 
+#define the switch function
+ontime = 200
+offtime = 100
+switch_function = vcat(ones(ontime,1), zeros(offtime),1)
+global switch_function = repeat(switch_function, (trunc(MC_steps/(ontime+offtime)) |> Int64)+1 ,1)
+global switch_function = switch_function[1:MC_steps]
+
 #-------------------------------------------------------------------#
 #monte carlo step
 function One_MC()
-    global r = rand(1:N)
+    #global r = rand(1:N)
 
-    rand_num = rand()
+    #rand_num = rand()
 
-    if rand_num < 0.5
-        global mx[r] = rand(1:50)
-    end
-end
-
-#-------------------------------------------------------------------#
-#auto-correlation functio 
-function correlation_numerator()
-    global cross_intensity_mx += mx_t .* mx_delt
-end
-
-function correlation_denominator()
-    global self_intensity_mx += mx
+    #if rand_num < 0.5
+    #    global mx[r] = rand(1:50)
+    #end
+    
+    global mx = rand(1:25)
 end
 
 #-------------------------------------------------------------------#
 #MATRIX FOR STORING DATA
-global mx_MCSteps = zeros(N, MC_steps) |> Array
+global mx_MCSteps = zeros(MC_steps, 1) |> Array
 
 #-------------------------------------------------------------------#
 #main body
 for steps in 1:MC_steps
     One_MC()
-    global mx_MCSteps[:,steps] = mx
+    global mx_MCSteps[steps, 1] = mx
 end
 
 #incorporate the grating function to Monte Carlo steps
-global mx_MCSteps = mx_MCSteps .* gating_function
+#global mx_MCSteps = mx_MCSteps .* gating_function
+#global mx_MCSteps = mx_MCSteps .* switch_function
+
+data = readdlm("ToyModel_DecorrelatedIntensity.txt")
+global mx_MCSteps = data
+global mx_MCSteps = mx_MCSteps .* switch_function
+global bin_intensity = mx_MCSteps
 
 #------------------------------------------------------------------------------------------------------------------------------#
 
@@ -68,40 +74,24 @@ global mx_MCSteps = mx_MCSteps .* gating_function
 global auto_correlation = zeros(length(delay_times), 1) |> Array
 
 #------------------------------------------------------------------------------------------------------------------------------#
-#calculating Auto-correlation
-global self_intensity_mx = sum(mx_MCSteps, dims=2)
+global denominator = (sum(bin_intensity)/length(bin_intensity)) .^2
 
 for delay in eachindex(delay_times)
+    global delay_time = delay_times[delay]
 
-    global interval_count = 0
+    global zero_mx = vec(zeros(delay_time, 1))
 
-    global cross_intensity_mx = zeros(N,1)
+    global bin_intensity_i = vcat(zero_mx, bin_intensity)
+    global bin_intensity_j = vcat(bin_intensity, zero_mx)
 
-    for i in 1:MC_steps
-        global mx_t = mx_MCSteps[:,i]
+    global cross_intensity = bin_intensity_i .* bin_intensity_j
+    
+    global g2 = (sum(cross_intensity)/(length(bin_intensity)-delay_time))/denominator
+    global auto_correlation[delay] = g2
 
-        if ((i+delay_times[delay]) <= MC_steps)
-            global mx_delt = mx_MCSteps[:, i+delay_times[delay]]
-            correlation_numerator()
-
-            global interval_count += 1
-        end
-    end
-    println(interval_count)
-    global g2_value = (cross_intensity_mx/interval_count) ./ ((self_intensity_mx/MC_steps) .^ 2)
-    global auto_correlation[delay] = sum(g2_value)/N
+    println(delay_time)
 end
-
-plot(delay_times, auto_correlation, framestyle=:box, xscale=:log10, 
-    linewidth=2, xlabel="Time delay", ylabel="Auto-correlation", legend=false,
-    minorticks=10, minorgrid=true)
-title!("Model#3")
-
-#savefig("AC_ToyModel_N$(N)_Grating1K.png")
-
-#SAVING THE GENERATED DATA
-#open("AC_ToyModel_N$(N)_Grating1K.txt", "w") do io 					#creating a file to save data
-#    for i in 1:length(delay_times)
-#       println(io,i,"\t", delay_times[i],"\t", auto_correlation[i])
-#    end
-#end
+plot(((delay_times[1:length(delay_times)]) .+1), (auto_correlation[1:length(auto_correlation)]), framestyle=:box, xscale=:log10, 
+    linewidth=2, xlabel="Time delay", ylabel="g2", label="Julia script(normalization=True)",
+    xminorticks=9, yminorticks=10, minorgrid=true, dpi=300)
+title!("ToyModel-Decorrelated data(no switch function)")
