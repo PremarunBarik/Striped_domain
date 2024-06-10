ff714872fb824cc05184840fcc87c71a639b91ef
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2024-06-10 18:46:35 -0500

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -8,23 +8,24 @@ df = pd.read_csv(file_path)
 timestamps = df.to_numpy()
 timestamps =  np.round( np.array(timestamps)/5120, 0)
 
-tt= timestamps[1:10000]
-binwidth = 1
-bins_tt = np.arange(tt.min(), tt.max(), binwidth) 
+tt= timestamps
+#tt = np.array(sorted(tt))
+tt = np.array(tt)
 
-tx, _ = np.histogram(tt, bins=bins_tt)
-C = np.correlate(tx, tx, mode='full')
+tau_array1 = np.arange(1,10,1)
+tau_array2 = np.arange(10,4001,10)
+tau_array = np.concatenate([tau_array1, tau_array2])
 
-Gn = C[tx.size:(tx.size+4000)]
-Gn_t = np.arange(1, Gn.size+1)
+lagbins = np.array(tau_array)
+G = pyc.pcorrelate(tt, tt, lagbins, normalize = False)
 
 fig, ax = plt.subplots(figsize=(10, 6))
-plt.plot(Gn_t, Gn, alpha=1, lw=2, label='numpy.correlate')
+plt.plot(lagbins[2:]*5120/1e8, G[1:], alpha=1, lw=2, label='numpy.pycorrelate')
 plt.xscale('log')
 plt.legend(loc='best', fontsize='x-large');
 
-#plt.show()
+plt.show()
 
-np.savetxt('Gn_t.txt', (Gn_t+1)*5120/1e8)
-np.savetxt('Gn.txt', Gn)
+np.savetxt('Gp_t_unsrt.txt', (lagbins[2:] )*5120/1e8)
+np.savetxt('Gp_unsrt.txt', G[1:])
 
